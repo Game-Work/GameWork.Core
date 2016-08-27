a0d8e2c3bcaae41d944a0823d247fc971dde3de1
Jared <jaredgglass@gmail.com>
2016-08-27 16:55:30 +0100

    audio interface

---
+++ GameWork/Audio/Interfaces/IAudioManager.cs
@@ -0,0 +1,15 @@
+﻿using GameWork.Controllers.Interfaces;
+
+namespace GameWork.Audio.Interfaces
+{
+	public interface IAudioController : IController
+	{
+		void Play(IAudioClip clip);
+
+		void Stop(IAudioClip clip);
+
+		void FadeIn(IAudioClip clip, float duration);
+
+		void FadeOut(IAudioClip clip, float duration);
+	}
+}
