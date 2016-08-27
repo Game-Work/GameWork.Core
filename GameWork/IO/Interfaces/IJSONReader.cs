f124615cd2fa60a1426790c8b9a6a104f9c2572e
Jared <jaredgglass@gmail.com>
2016-08-27 16:55:15 +0100

    json model loader interface

---
+++ GameWork/IO/Interfaces/IJSONReader.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+using GameWork.Models.Interfaces;
+
+namespace GameWork.IO.Interfaces
+{
+	public interface IJsonReader
+	{
+		TModel ConstructModel<TModel>(Stream stream) where TModel : IModel;
+	}
+}
