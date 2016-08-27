54057d9093c3e48f14b4144c1eebef94dbda62c0
Jared <jaredgglass@gmail.com>
2016-08-27 16:55:49 +0100

    controllers

---
+++ GameWork/Controllers/Controller.cs
@@ -0,0 +1,35 @@
+﻿using GameWork.Controllers.Interfaces;
+
+namespace GameWork.Controllers
+{
+	public abstract class Controller : IController
+	{
+		public virtual bool IsActive { get; private set; }
+
+		public virtual void Initialize()
+		{			
+		}
+
+		public virtual void Activate()
+		{
+			IsActive = true;
+		}
+
+		public virtual void Tick(float deltaTime)
+		{
+		}
+
+		public virtual void Deactivate()
+		{
+			IsActive = false;
+		}
+
+		public virtual void Terminate()
+		{
+			if(!IsActive)
+			{
+				Deactivate();
+			}
+		}
+	}
+}
---
+++ GameWork/Controllers/Interfaces/IController.cs
@@ -0,0 +1,17 @@
+﻿namespace GameWork.Controllers.Interfaces
+{
+	public interface IController
+	{
+		bool IsActive { get; }
+
+		void Initialize();
+
+		void Activate();
+
+		void Tick(float deltaTime);
+
+		void Deactivate();
+
+		void Terminate();
+	}
+}
