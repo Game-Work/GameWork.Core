b8ed338513b75d63647997de4b6bbdeb61ed08fc
Jared Glass <jared@playgen.com>
2016-09-08 15:47:14 +0100

    merge

--- Interfacing/StateInterface.cs
+++ Interfacing/StateInterface.cs
@@ -4,37 +4,47 @@ using GameWork.Commands.Interfaces;
 
 namespace GameWork.Interfacing
 {
-    public abstract class StateInterface : ICommandInterface
-    {
-        private readonly CommandQueue _commandQueue = new CommandQueue();
-        
-        public bool HasCommands
-        {
-            get { return _commandQueue.HasCommands; }
-        }
+	public abstract class StateInterface : ICommandInterface
+	{
+		private readonly CommandQueue _commandQueue = new CommandQueue();
+		
+		public bool HasCommands
+		{
+			get { return _commandQueue.HasCommands; }
+		}
 
-        public ICommand TakeFirstCommand()
-        {
-            return _commandQueue.TakeFirstCommand();
-        }
+		public ICommand TakeFirstCommand()
+		{
+			return _commandQueue.TakeFirstCommand();
+		}
 
-        public ICommand[] TakeAllCommands()
-        {
-            return TakeAllCommands();
-        }
-        
-        public abstract void Enter();
+		public ICommand[] TakeAllCommands()
+		{
+			return TakeAllCommands();
+		}
+		
+		public abstract void Enter();
 
-        public abstract void Exit();
+		public abstract void Exit();
 
-        protected void EnqueueCommand(ICommand command)
-        {
-            _commandQueue.AddCommand(command);
-        }
+		public virtual void Initialize()
+		{
+			
+		}
 
-        protected void EnqueueCommands(IEnumerable<ICommand> commands)
-        {
-            _commandQueue.AddCommands(commands);
-        }
-    }
+		public virtual void Terminate()
+		{
+			
+		}
+
+		protected void EnqueueCommand(ICommand command)
+		{
+			_commandQueue.AddCommand(command);
+		}
+
+		protected void EnqueueCommands(IEnumerable<ICommand> commands)
+		{
+			_commandQueue.AddCommands(commands);
+		}
+	}
 }
--- States/StateController.cs
+++ States/StateController.cs
@@ -4,53 +4,61 @@ using GameWork.States.Interfaces;
 
 namespace GameWork.States
 {
-    public class StateController : IController
-    {
-        private readonly Dictionary<string, IState> _states  = new Dictionary<string, IState>();
-        private string _activeState;
+	public class StateController<TState> : IController
+		where TState : IState
+	{
+		protected readonly Dictionary<string, TState> _states  = new Dictionary<string, TState>();
+		protected string _activeState;
 
-        public StateController(params IState[] states)
-        {
-            foreach (var state in states)
-            {
-                _states.Add(state.Name, state);
-            }
-        }
+		public StateController(params TState[] states)
+		{
+			foreach (var state in states)
+			{
+				_states.Add(state.Name, state);
+			}
+		}
 
-        public void SetState(string name)
-        {
-            var newState = _states[name];
-            _activeState = name;
-            newState.ChangeStateEvent += ChangeState;
-            newState.Enter();
-        }
+		public void SetState(string name)
+		{
+			var newState = _states[name];
+			_activeState = name;
+			newState.ChangeStateEvent += ChangeState;
+			newState.Enter();
+		}
 
-        public void ChangeState(string name)
-        {
-            var newState = _states[name];
-            var prevState = _states[_activeState];
+		public void ChangeState(string name)
+		{
+			var newState = _states[name];
+			var prevState = _states[_activeState];
 
-            _activeState = name;
+			_activeState = name;
 
-            prevState.ChangeStateEvent -= ChangeState;
-            newState.ChangeStateEvent += ChangeState;
+			prevState.ChangeStateEvent -= ChangeState;
+			newState.ChangeStateEvent += ChangeState;
 
-            prevState.Exit();
-            newState.Enter();
-        }
+			prevState.Exit();
+			newState.Enter();
+		}
 
-        public void Tick(float deltaTime)
-        {
-            _states[_activeState].Tick(deltaTime);
-        }
+		public void Tick(float deltaTime)
+		{
+			_states[_activeState].Tick(deltaTime);
+		}
 
-        public void Initialize()
-        {
-        }
+		public void Initialize()
+		{
+			foreach (var state in _states.Values)
+			{
+				state.Initialize();
+			}
+		}
 
-        public void Terminate()
-        {
-            _states = null;
-        }
-    }
+		public void Terminate()
+		{
+			foreach (var state in _states.Values)
+			{
+				state.Terminate();
+			}
+		}
+	}
 }
