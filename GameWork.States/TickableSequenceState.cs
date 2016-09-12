11e023c8f5fe4213473d2e98d7be95aa4fdd85d1
Jared Glass <jared@playgen.com>
2016-09-12 19:08:43 +0100

    tickable sequence state

---
+++ GameWork.States/TickableSequenceState.cs
@@ -0,0 +1,9 @@
+﻿using GameWork.States.Interfaces;
+
+namespace GameWork.States
+{
+    public abstract class TickableSequenceState : SequenceState, ITickableState
+    {
+        public abstract void Tick(float deltaTime);
+    }
+}
