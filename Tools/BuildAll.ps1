04a31ba901aa7212dcc233841ba9232c00b28565
Jared <jaredgglass@gmail.com>
2017-05-25 14:44:18 +0100

    changed to use devenv to build for vs 2017 compatibility

--- Tools/BuildAll.ps1
+++ Tools/BuildAll.ps1
@@ -1,8 +1,8 @@
 # Must be executed from the Tools folder
 
-$msbuild = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+$devenv = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv"
 
 $nuget = ".\nuget.exe"
 
 & $nuget restore "..\GameWork.Core.sln"
-& $msbuild "..\GameWork.Core.sln"
+& $devenv "..\GameWork.Core.sln" /build
